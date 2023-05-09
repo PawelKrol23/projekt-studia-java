e01db90d596af120c5989c677f0970507e3221ec
PawelKrol23 <p.krol18a@gmail.com>
2023-05-09 16:55:50 +0200

    Initial commit

---
+++ src/main/java/com/example/projekt_studia_java/ProjektStudiaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projekt_studia_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektStudiaJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektStudiaJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projekt_studia_java/ProjektStudiaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projekt_studia_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektStudiaJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
