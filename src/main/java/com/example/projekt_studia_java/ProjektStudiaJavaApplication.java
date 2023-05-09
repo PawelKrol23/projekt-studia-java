5cc1b96253a53753aa38eed7ec1ad891c1f57edd
LukaszPB <lukasz.kiersnowski.110752@student.pb.edu.pl>
2023-05-09 17:23:24 +0200

    komentarz

--- src/main/java/com/example/projekt_studia_java/ProjektStudiaJavaApplication.java
+++ src/main/java/com/example/projekt_studia_java/ProjektStudiaJavaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjektStudiaJavaApplication {
 
+    //komentarz
     public static void main(String[] args) {
         SpringApplication.run(ProjektStudiaJavaApplication.class, args);
     }
