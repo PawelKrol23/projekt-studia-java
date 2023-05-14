76486dc6b9bb685b82e1602d9fedd70b243662ef
Lisu02 <104763694+Lisu02@users.noreply.github.com>
2023-05-14 19:46:25 +0200

    Test commita i Push

--- src/main/java/com/example/projekt_studia_java/ProjektStudiaJavaApplication.java
+++ src/main/java/com/example/projekt_studia_java/ProjektStudiaJavaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjektStudiaJavaApplication {
-
+    //komentarz Adrian test
     //komentarz
     public static void main(String[] args) {
         SpringApplication.run(ProjektStudiaJavaApplication.class, args);
