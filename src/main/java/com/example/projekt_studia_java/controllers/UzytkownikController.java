01583e2f3fd41922c1201bd8e93b1704c72aee30
lewy212 <huak@wp.pl>
2023-05-25 14:46:34 +0200

    uzytkownik ma walidacje

--- src/main/java/com/example/projekt_studia_java/controllers/UzytkownikController.java
+++ src/main/java/com/example/projekt_studia_java/controllers/UzytkownikController.java
@@ -3,9 +3,11 @@ package com.example.projekt_studia_java.controllers;
 
 import com.example.projekt_studia_java.domain.Uzytkownik;
 import com.example.projekt_studia_java.services.UzytkownikService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,13 +27,21 @@ public class UzytkownikController {
     }
     @GetMapping("/zarejestruj")
     public String formularzDodawaniaUzytkownika(Model model) {
-        Uzytkownik uzytkownik = new Uzytkownik();
-        model.addAttribute("uzytkownicy",uzytkownik);
+        //Uzytkownik uzytkownik = new Uzytkownik();
+        model.addAttribute("newUzytkownik",new Uzytkownik());
         return "zarejestruj";
     }
     @PostMapping("/zarejestruj")
-    public String dodajUzytkownika(@ModelAttribute Uzytkownik uzytkownik){
-        uzytkownikService.zapisz(uzytkownik);
+    public String dodajUzytkownika(@Valid @ModelAttribute("newUzytkownik") Uzytkownik newUzytkownik, BindingResult result, Model model){
+        if(result.hasErrors())
+        {
+           System.out.println(result.getErrorCount());
+           result.getAllErrors().forEach(el->System.out.println(el));
+           return"zarejestruj";
+        }
+
+        uzytkownikService.zapisz(newUzytkownik);
+
         return "redirect:/uzytkownik";
     }
     @GetMapping("/zaloguj")
--- src/main/java/com/example/projekt_studia_java/domain/Uzytkownik.java
+++ src/main/java/com/example/projekt_studia_java/domain/Uzytkownik.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Data
@@ -15,11 +16,24 @@ public class Uzytkownik {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Size(min=3,max=20,message = "Imie musi miec od 3 do 20 liter ")
+    @Pattern(regexp = "^[A-Z][a-z]*$",message = "Imie musi sie zaczynac wielka litera")
     private String imie;
+    @Size(min=3,max=50,message = "Nazwisko musi miec od 3 do 50 liter ")
+    @Pattern(regexp = "^[A-Z][a-z]*$",message = "Nazwisko musi sie zaczynac wielka litera")
     private String nazwisko;
+    @Size(min=3,max=20,message = "login musi miec od 3 do 20 znakow")
+    @Pattern(regexp = "^[a-z]+$",message = "login musi zawierac tylko male litery")
     private String login;
+
+    @Size(min=5,message = "haslo musi zawierac conajmniej 5 znakow")
     private String haslo;
+
+    @Email
     private String mail;
+    @Min(value = 18,message = "minimalny wiek to 18 lat")
+    @NotNull
     private Integer wiek;
 
 }
