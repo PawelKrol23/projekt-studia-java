ab3267e821baba302f26e8e70f9a7826e0603f6a
lewy212 <huak@wp.pl>
2023-05-26 00:51:38 +0200

    walidacje do informacji poprawka

--- src/main/java/com/example/projekt_studia_java/domain/Informacja.java
+++ src/main/java/com/example/projekt_studia_java/domain/Informacja.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import jakarta.validation.constraints.*;
 import org.hibernate.validator.constraints.URL;
+import org.springframework.data.domain.DomainEvents;
 
 @Data
 @AllArgsConstructor
@@ -14,8 +15,9 @@ public class Informacja {
     @Size(min=3,max=20,message = "Nazwa musi miec od 3 do 20 liter")
     private String tytul;
     private String kategoria;
-    @Size(min=5,max=500,message = "Nazwa musi miec od 5 do 500 liter")
+    @Size(min=5,max=500,message = "Tresc musi miec od 5 do 500 liter")
     private String tresc;
 
+    @NotBlank(message="Link nie moze byc pusty")
     private String link;
 }
