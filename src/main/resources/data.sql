39f9376bbe03b52edeb3aa10b39bc2ed1ff05bb4
lewy212 <huak@wp.pl>
2023-06-15 01:05:01 +0200

    user_weak + 2 admin dla wiekszej liczby uzytkownikow

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -23,12 +23,20 @@ VALUES ('2022-05-12 00:00:00', '2022-06-12', 'www.kompletnie_inny_link.com', 'sz
 
 -- Uzytkownicy
 INSERT INTO Uzytkownik (imie, nazwisko, login, haslo, mail, wiek)
-VALUES ('szef', 'szefu', 'admin', 'admin', 'mail@admina.pl', 28);
+VALUES ('Szef', 'Szefu', 'admin', 'admin', 'mail@admina.pl', 28);
 INSERT INTO Uzytkownik (imie, nazwisko, login, haslo, mail, wiek)
-VALUES ('szefito', 'szefito', 'user', 'user', 'mail@user.pl', 20);
+VALUES ('Adminek', 'Adminekv2', 'admin2', 'admin2', 'mail@admina2.pl', 20);
+INSERT INTO Uzytkownik (imie, nazwisko, login, haslo, mail, wiek)
+VALUES ('Szefito', 'Szefito', 'user', 'user', 'mail@user.pl', 20);
+INSERT INTO Uzytkownik (imie, nazwisko, login, haslo, mail, wiek)
+VALUES ('Jestem_weak', 'Weaked', 'user_weak', 'user_weak', 'mail@user.pl', 29);
 
 --Role
 INSERT INTO Role (rola, Uzytkownik_id)
 VALUES ('ADMIN', SELECT id FROM Uzytkownik WHERE login = 'admin');
 INSERT INTO Role (rola, Uzytkownik_id)
+VALUES ('ADMIN', SELECT id FROM Uzytkownik WHERE login = 'admin2');
+INSERT INTO Role (rola, Uzytkownik_id)
 VALUES ('USER', SELECT id FROM Uzytkownik WHERE login = 'user');
+INSERT INTO Role (rola, Uzytkownik_id)
+VALUES ('USER_WEAK', SELECT id FROM Uzytkownik WHERE login = 'user_weak');
