67d794deb011efc7d067ccd1403ff44b256c7e89
PawelKrol23 <p.krol18a@gmail.com>
2023-06-16 12:21:20 +0200

    Lekka zmiana do walidacji

--- src/main/java/com/example/projekt_studia_java/domain/db/UzytkownikEntity.java
+++ src/main/java/com/example/projekt_studia_java/domain/db/UzytkownikEntity.java
@@ -18,22 +18,23 @@ public class UzytkownikEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Size(min=3,max=20,message = "Imie musi miec od 3 do 20 liter ")
-    @Pattern(regexp = "^[A-Z][a-z]*$",message = "Imie musi sie zaczynac wielka litera")
+    @Size(min=3,max=20,message = "Imie musi miec od 3 do 20 liter")
+    @Pattern(regexp = "^[A-Z][a-z]*$",message = "Imie musi sie zaczynac wielka litera i skladac z samych liter")
     private String imie;
     @Size(min=3,max=50,message = "Nazwisko musi miec od 3 do 50 liter ")
-    @Pattern(regexp = "^[A-Z][a-z]*$",message = "Nazwisko musi sie zaczynac wielka litera")
+    @Pattern(regexp = "^[A-Z][a-z]*$",message = "Nazwisko musi sie zaczynac wielka litera i skladac z samych liter")
     private String nazwisko;
     @Column(name="login" , nullable = false, length = 50)
     @Size(min=3,max=20,message = "login musi miec od 3 do 20 znakow")
-    @Pattern(regexp = "^[a-z]+$",message = "login musi zawierac tylko male litery")
+    @Pattern(regexp = "^[a-z]+$",message = "login moze zawierac tylko male litery")
     private String login;
 
     @Column(name="haslo" , nullable = false, length = 200)
     @Size(min=5,message = "haslo musi zawierac conajmniej 5 znakow")
     private String haslo;
 
-    @Email
+    @Email(message = "Email niepoprawny")
+    @NotNull(message = "Email nie moze byc pusty")
     private String mail;
     @Min(value = 18,message = "minimalny wiek to 18 lat")
     @NotNull
